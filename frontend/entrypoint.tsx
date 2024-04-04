98cc31d8c1e87b5416a2333cda58d1b09878cd8c
Jonas Strehle <jonas@strehles.info>
2024-04-04 13:25:13 +0200

    Work on history API

--- frontend/entrypoint.tsx
+++ frontend/entrypoint.tsx
@@ -1,17 +1,41 @@
-const content = $$("Qui irure Lorem non voluptate pariatur dolor.");
-const textarea = <textarea rows="4" cols="50"/> as HTMLTextAreaElement;
+import { Icon } from "components/Defaults/Icon.tsx";
+import { History } from "datex-core-legacy/utils/history.ts";
+
+// Initializing a pointer with the content of the LoremIpsum file
+const content = $$<string>(await datex.get("./LoremIpsum.txt"));
+
+// Creating history object
+const history = new History();
+history.add(content);
+
+// Textarea element
+const textarea = <textarea/> as HTMLTextAreaElement;
 
 effect(()=>{
+	// Effect gets triggered on pointer change
 	textarea.value = val(content);
-	console.log("change", content.val)
 })
 textarea.addEventListener("input", (e) => {
-	content.setVal(textarea.value)
+	// Event gets triggered on area input
+	content.setVal(textarea.value);
 	e.preventDefault();
 	e.stopPropagation();
 })
-globalThis.content = content;
+
 export default
 	<main>
+		<header>
+			<span onclick:frontend={() => history.back()}>
+				Undo ({always(()=>(val(content), history.backSteps))})
+				<Icon name="fa-undo"/>
+			</span>
+			<span onclick:frontend={() => history.forward()}>
+				Redo ({always(()=>(val(content), history.forwardSteps))})
+				<Icon name="fa-redo"/>
+			</span>
+		</header>
 		{textarea}
 	</main>
+
+// @ts-ignore $
+globalThis.content = content;
