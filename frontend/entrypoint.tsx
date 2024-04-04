c8e582efc713d4dabec3fe0217e9d222b2b04f02
Jonas Strehle <jonas@strehles.info>
2024-04-04 13:11:17 +0200

    Work on history API

--- frontend/entrypoint.tsx
+++ frontend/entrypoint.tsx
@@ -1,14 +1,16 @@
 const content = $$("Qui irure Lorem non voluptate pariatur dolor.");
-const textarea = <textarea rows="4" cols="50">
-	{always(()=>{
-		return val(content);
-	})}
-</textarea> as HTMLTextAreaElement;
-textarea.addEventListener("change", () => {
-	content.setVal(textarea.value, false)
+const textarea = <textarea rows="4" cols="50"/> as HTMLTextAreaElement;
+
+effect(()=>{
+	textarea.value = val(content);
+	console.log("change", content.val)
+})
+textarea.addEventListener("input", (e) => {
+	content.setVal(textarea.value)
+	e.preventDefault();
+	e.stopPropagation();
 })
 globalThis.content = content;
-
 export default
 	<main>
 		{textarea}
