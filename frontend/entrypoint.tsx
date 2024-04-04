06ccce3b65f7abad2e5f99f8c83e319b3c0662c1
Jonas Strehle <jonas@strehles.info>
2024-04-04 13:40:54 +0200

    Add deployment

--- frontend/entrypoint.tsx
+++ frontend/entrypoint.tsx
@@ -4,12 +4,17 @@ import { History } from "datex-core-legacy/utils/history.ts";
 // Initializing a pointer with the content of the LoremIpsum file
 const content = $$<string>(await datex.get("./LoremIpsum.txt"));
 
-// Creating history object
-const history = new History();
-history.add(content);
+// Creating history object with autosave
+const autoHistory = new History();
+autoHistory.add(content);
+
+
+// Creating history object without autosave
+const manualHistory = new History({explicitSavePoints: true});
+manualHistory.add(content);
 
 // Textarea element
-const textarea = <textarea/> as HTMLTextAreaElement;
+const textarea = <textarea spellcheck="false"/> as HTMLTextAreaElement;
 
 effect(()=>{
 	// Effect gets triggered on pointer change
@@ -25,14 +30,33 @@ textarea.addEventListener("input", (e) => {
 export default
 	<main>
 		<header>
-			<span onclick:frontend={() => history.back()}>
-				Undo ({always(()=>(val(content), history.backSteps))})
-				<Icon name="fa-undo"/>
-			</span>
-			<span onclick:frontend={() => history.forward()}>
-				Redo ({always(()=>(val(content), history.forwardSteps))})
-				<Icon name="fa-redo"/>
-			</span>
+			<div>
+				<h1>Auto</h1>
+				<span onclick:frontend={() => autoHistory.back()}>
+					Undo ({always(()=>(val(content), autoHistory.backSteps))})
+					<Icon name="fa-undo"/>
+				</span>
+				<span onclick:frontend={() => autoHistory.forward()}>
+					Redo ({always(()=>(val(content), autoHistory.forwardSteps))})
+					<Icon name="fa-redo"/>
+				</span>
+			</div>
+
+			<div>
+				<h1>Manual</h1>
+				<span onclick:frontend={() => manualHistory.setSavePoint()}>
+					Save
+					<Icon name="fa-save"/>
+				</span>
+				<span onclick:frontend={() => manualHistory.back()}>
+					Undo ({always(()=>(val(content), manualHistory.backSteps))})
+					<Icon name="fa-undo"/>
+				</span>
+				<span onclick:frontend={() => manualHistory.forward()}>
+					Redo ({always(()=>(val(content), manualHistory.forwardSteps))})
+					<Icon name="fa-redo"/>
+				</span>
+			</div>
 		</header>
 		{textarea}
 	</main>
