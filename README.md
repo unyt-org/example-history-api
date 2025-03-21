a3b9c379273d696e1df9b2f52f1f23a042282ffb
Jonas Strehle <jonas@strehles.info>
2025-03-21 20:43:27 +0100

    Upgrade to UIX@4

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # DATEX History API Example
 
-This is a example project demonstrating how to use the [History API](https://docs.unyt.org/manual/datex/history) of DATEX.
-The API provides a way to undo and redo state changes of one or multiple pointers.
+This is a example project demonstrating how to use the
+[History API](https://docs.unyt.org/manual/datex/history) of DATEX. The API
+provides a way to undo and redo state changes of one or multiple pointers.
 
 ## Usage
-The history for a pointer can be managed by creating a new History object and adding the pointer to the history:
+
+The history for a pointer can be managed by creating a new History object and
+adding the pointer to the history:
 
 ```ts
 import { History } from "datex-core-legacy/utils/history.ts";
@@ -14,28 +17,28 @@ const history = new History();
 history.add(entries);
 ```
 
-Now, every state change of the entries array is recorded and can be undone or repeated:
+Now, every state change of the entries array is recorded and can be undone or
+repeated:
 
 ```ts
 entries.push("Entry 1");
 entries.push("Entry 2");
 entries.push("Entry 3");
-console.log(entries) // ["Entry 1", "Entry 2", "Entry 3"]
+console.log(entries); // ["Entry 1", "Entry 2", "Entry 3"]
 
 // undo last change
 history.back();
-console.log(entries) // ["Entry 1", "Entry 2"]
+console.log(entries); // ["Entry 1", "Entry 2"]
 
 // undo second last change
 history.back();
-console.log(entries) // ["Entry 1"]
+console.log(entries); // ["Entry 1"]
 
 // repeat second last change
 history.forward();
-console.log(entries) // ["Entry 1", "Entry 2"]
+console.log(entries); // ["Entry 1", "Entry 2"]
 ```
-
 
 ---
 
-<sub>&copy; unyt 2024 • [unyt.org](https://unyt.org)</sub>
+<sub>&copy; unyt 2025 • [unyt.org](https://unyt.org)</sub>
--- frontend/entrypoint.tsx
+++ frontend/entrypoint.tsx
@@ -1,68 +1,65 @@
-import { Icon } from "components/Defaults/Icon.tsx";
-import { Textarea } from "./Textarea.tsx";
 import { History } from "datex-core-legacy/utils/history.ts";
 import { Entrypoint } from "uix/providers/entrypoints.ts";
 
 // Initializing a pointer with the content of the LoremIpsum file
-const content = $$<string>(await datex.get("./LoremIpsum.txt"));
+const content = $<string>(await datex.get("./LoremIpsum.txt"));
 
 // Creating history object with autosave
 const autoHistory = new History();
-const autoTextarea = <Textarea value={content}/>;
+const autoTextarea = <textarea value={content} />;
 autoHistory.add(content);
 
 // Creating history object without autosave
-const manualHistory = new History({explicitSavePoints: true});
-const manualTextarea = <Textarea value={content}/>;
+const manualHistory = new History({ explicitSavePoints: true });
+const manualTextarea = <textarea value={content} />;
 manualHistory.add(content);
 
 export default {
-	// Redirect to /auto route per default
-	'/': new URL("/auto", location.origin),
+  // Redirect to /auto route per default
+  "/": new URL("/auto", location.origin),
 
-	// Handling /auto route
-	'/auto': <main>
-		<header>
-			<div>
-				<h1>Auto</h1>
-				<span onclick:frontend={() => autoHistory.back()}>
-					Undo ({always(()=>(val(content), autoHistory.backSteps))})
-					<Icon name="fa-undo"/>
-				</span>
-				<span onclick:frontend={() => autoHistory.forward()}>
-					Redo ({always(()=>(val(content), autoHistory.forwardSteps))})
-					<Icon name="fa-redo"/>
-				</span>
-			</div>
-			<div>
-				Go to <a href="/manual">/manual</a>
-			</div>
-		</header>
-		{autoTextarea}
-	</main>,
+  // Handling /auto route
+  "/auto": (
+    <main>
+      <header>
+        <div>
+          <h1>Auto</h1>
+          <span onclick:frontend={() => autoHistory.back()}>
+            Undo ({always(() => (val(content), autoHistory.backSteps))})
+          </span>
+          <span onclick:frontend={() => autoHistory.forward()}>
+            Redo ({always(() => (val(content), autoHistory.forwardSteps))})
+          </span>
+        </div>
+        <div>
+          Go to <a href="/manual">/manual</a>
+        </div>
+      </header>
+      {autoTextarea}
+    </main>
+  ),
 
-	// Handle /manual route
-	'/manual': <main>
-		<header>
-			<div>
-				<h1>Manual</h1>
-				<span onclick:frontend={() => manualHistory.setSavePoint()}>
-					Save
-					<Icon name="fa-save"/>
-				</span>
-				<span onclick:frontend={() => manualHistory.back()}>
-					Undo ({always(()=>(val(content), manualHistory.backSteps))})
-					<Icon name="fa-undo"/>
-				</span>
-				<span onclick:frontend={() => manualHistory.forward()}>
-					Redo ({always(()=>(val(content), manualHistory.forwardSteps))})
-					<Icon name="fa-redo"/>
-				</span>
-			</div>
-			<div>
-				Go to <a href="/auto">/auto</a>
-			</div>
-		</header>
-		{manualTextarea}
-	</main>
+  // Handle /manual route
+  "/manual": (
+    <main>
+      <header>
+        <div>
+          <h1>Manual</h1>
+          <span onclick:frontend={() => manualHistory.setSavePoint()}>
+            Save
+          </span>
+          <span onclick:frontend={() => manualHistory.back()}>
+            Undo ({always(() => (val(content), manualHistory.backSteps))})
+          </span>
+          <span onclick:frontend={() => manualHistory.forward()}>
+            Redo ({always(() => (val(content), manualHistory.forwardSteps))})
+          </span>
+        </div>
+        <div>
+          Go to <a href="/auto">/auto</a>
+        </div>
+      </header>
+      {manualTextarea}
+    </main>
+  ),
 } satisfies Entrypoint;
